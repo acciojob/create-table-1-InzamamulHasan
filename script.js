9de9d5ca56578a3adc0ee9809334b5c7d5aa7cf6
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-05-01 18:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let c1=2;
 function insert_Row() {
     //Write your code here
-  
+	let table=document.getElementById("sampleTable");
+
+	let tablerow=table.insertRow(0);
+	let cell1=tablerow.insertCell(0);
+	let cell2=tablerow.insertCell(1);
+
+	cell1.innerHTML="New Cell1";
+	cell2.innerHTML="New Cell2";
   
 }
